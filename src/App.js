c81141a1aa965d19138b05c85cbf3e8b812c2cee
KatyRosli <96566968+KatyRosli@users.noreply.github.com>
2022-03-08 17:37:29 +0100

    created calculator app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="Calculator React App"></header>
+      Hello React JS
+    </div>
+  );
+}
+
+export default App;
