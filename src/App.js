8a1cb9642121e43ec33a747a517c15e10b12882b
KatyRosli <96566968+KatyRosli@users.noreply.github.com>
2022-03-12 07:28:13 +0100

    add function for input number

--- src/App.js
+++ src/App.js
@@ -1,7 +1,98 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [preState, setPreState] = useState("");
+  const [curState, setCurState] = useState("");
+  const [input, setInput] = useState("0");
+  const [operator, setOperator] = useState(null);
+  const [total, setTotal] = useState(false);
+
+  const inputNum = (e) => {
+    if (curState.includes(".") && e.target.innerText === ".") return;
+
+    if (total) {
+      setPreState("");
+    }
+    curState
+      ? setCurState((pre) => pre + e.target.innerText)
+      : setCurState(e.target.innerText);
+    setTotal(false);
+  };
+
+  const operatorType = (e) => {};
+
+  const equals = (e) => {};
+
+  const minusPlus = () => {};
+
+  const percent = () => {};
+
+  const reset = () => {};
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="screen"></div>
+        <div className="btn" onClick={reset}>
+          AC
+        </div>
+        <div className="btn" onClick={percent}>
+          %
+        </div>
+        <div className="btn" onClick={minusPlus}>
+          +/-
+        </div>
+        <div className="btn" onClick={operatorType}>
+          /
+        </div>
+        <div className="btn" onClick={inputNum}>
+          7
+        </div>
+        <div className="btn" onClick={inputNum}>
+          8
+        </div>
+        <div className="btn" onClick={inputNum}>
+          9
+        </div>
+        <div className="btn" onClick={operatorType}>
+          X
+        </div>
+        <div className="btn" onClick={inputNum}>
+          4
+        </div>
+        <div className="btn" onClick={inputNum}>
+          5
+        </div>
+        <div className="btn" onClick={inputNum}>
+          6
+        </div>
+        <div className="btn" onClick={operatorType}>
+          +
+        </div>
+        <div className="btn" onClick={inputNum}>
+          1
+        </div>
+        <div className="btn" onClick={inputNum}>
+          2
+        </div>
+        <div className="btn" onClick={inputNum}>
+          3
+        </div>
+        <div className="btn" onClick={operatorType}>
+          -
+        </div>
+        <div className="btn" onClick={inputNum}>
+          0
+        </div>
+        <div className="btn" onClick={inputNum}>
+          .
+        </div>
+        <div className="btn" onClick={equals}>
+          =
+        </div>
+      </div>
+    </div>
+  );
 }
 export default App;
